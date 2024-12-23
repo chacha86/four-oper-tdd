edf86bf931a5abea599dca2f854994ac7af9c2f1
chacha86 <taejincha0619@gmail.com>
2024-12-23 13:37:02 +0900

    work

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,17 +5,20 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 
     @Test
+    @DisplayName("테스트 함수 실행")
     public void test1() {
         System.out.println("test1");
     }
 
     @Test
+    @DisplayName("plus 함수 구현")
     public void t2() {
         Calculator calculator = new Calculator();
         calculator.plus(1, 2);
     }
 
     @Test
+    @DisplayName("plus 함수 1 + 2 테스트")
     public void t3() {
         Calculator calculator = new Calculator();
         int rst = calculator.plus(1, 2);
